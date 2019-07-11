9f7e5f85f3de205e4e370b6490b74125f161b111
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-07-11 15:19:58 +0700

    Set up themeleaf with default index.html

--- src/main/java/com/example/ruarduan_web/RuarduanWebApplication.java
+++ src/main/java/com/example/ruarduan_web/RuarduanWebApplication.java
@@ -1,8 +1,10 @@
 package com.example.ruarduan_web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RuarduanWebApplication {
 
