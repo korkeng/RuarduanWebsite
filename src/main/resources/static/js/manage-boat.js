126c4a4410f973c24bac863b6e6bf9bcad28d283
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-08-30 15:44:26 +0700

    [ADD] route table on boat management

--- src/main/resources/static/js/manage-boat.js
+++ src/main/resources/static/js/manage-boat.js
@@ -1,27 +1,26 @@
-var tableData;
-
-$(document).ready(function(){
-	$.ajax({
+$(document).ready(function() {
+    $.ajax({
         type: "GET",
         url: "http://139.99.117.190:8080/boattypes",
         dataType: 'json',
         success: function (data) {
             var dataHeader = ["<b>Boattype ID</b>","<b>Language</b>","<b>Name</b>","<b>Edit</b>","<b>Delete</b>"];
             var cellHeader = [];
-            document.getElementById("tableHeaderBoat").innerHTML = "Boattype Table";
-                var header = document.getElementById("myThHeaderBoat");
+            document.getElementById("tableHeader").innerHTML = "Boattype Table";
+            if (document.getElementById("dataTable").rows.length == 0) {
+                var header = document.getElementById("myThHeader");
                 var rowHeader = header.insertRow(0);
                 for (i = 0; i < dataHeader.length; i++) {
                     cellHeader[i] = rowHeader.insertCell(i);
                     cellHeader[i].innerHTML = ""+dataHeader[i];
                 }
-                var footer = document.getElementById("myThFooterBoat");
+                var footer = document.getElementById("myThFooter");
                 var rowFooter = footer.insertRow(0);
                 for (i = 0; i < dataHeader.length; i++) {
                     cellHeader[i] = rowFooter.insertCell(i);
                     cellHeader[i].innerHTML = ""+dataHeader[i];
                 }
-                var table = document.getElementById("myThBodyBoat");
+                var table = document.getElementById("myThBody");
                 var cellData = [];
                 for (i = data.length-1; i > -1; i--) {
                     var row1 = table.insertRow(0);
@@ -46,19 +45,269 @@ $(document).ready(function(){
                         } 
                     }        
                 }
-                tableData = $('#dataTableBoat').DataTable();
-                // dataLength = data.length;
-			},
-			error: function(e){
-				console.log("Error!"+e);
-			}
-	   });
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD BOATTYPE";
+            } else {
+                tableData.destroy();
+                for (i = 0; i < dataLength+2; i++) {
+                    document.getElementById("dataTable").deleteRow(0);
+                }
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattype_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattypeLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].name;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].boattypeId+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].boattypeId+")\">Delete</button></span>";
+                                break;
+                        }
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD BOATTYPE";
+            }   
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
     });
+    
+});
 
-	function editRole(Id){
+var tableData;
+var dataLength;
+function changeViewBoattype() {
+       $.ajax({
+        type: "GET",
+        url: "http://139.99.117.190:8080/boattypes",
+        dataType: 'json',
+        success: function (data) {
+            var dataHeader = ["<b>Boattype ID</b>","<b>Language</b>","<b>Name</b>","<b>Edit</b>","<b>Delete</b>"];
+            var cellHeader = [];
+            document.getElementById("tableHeader").innerHTML = "Boattype Table";
+            if (document.getElementById("dataTable").rows.length == 0) {
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattype_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattypeLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].name;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].boattypeId+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].boattypeId+")\">Delete</button></span>";
+                                break;
+                        } 
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length;  
+                document.getElementById("link").innerHTML = "ADD BOATTYPE";
+                 
+            } else {
+                tableData.destroy();
+                for (i = 0; i < dataLength+2; i++) {
+                    document.getElementById("dataTable").deleteRow(0);
+                }
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattype_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].boattypeId.boattypeLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].name;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].boattypeId+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].boattypeId+")\">Delete</button></span>";
+                                break;
+                        }
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD BOATTYPE";
+            }   
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+}
 
-		alert("Id"+Id.toString());
-	}
-	function deleteRole(Id){
-		alert("Id2"+Id.toString());
-	}
+function changeViewRoute() {
+       $.ajax({
+        type: "GET",
+        url: "http://139.99.117.190:8080/routes",
+        dataType: 'json',
+        success: function (data) {
+            var dataHeader = ["<b>Route ID</b>","<b>Language</b>","<b>Route Name</b>","<b>Edit</b>","<b>Delete</b>"];
+            var cellHeader = [];
+            document.getElementById("tableHeader").innerHTML = "Route Table";
+            if (document.getElementById("dataTable").rows.length == 0) {
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].routeId.route_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].routeId.routeLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].routeName;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId+")\">Delete</button></span>";
+                                break;
+                        } 
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length;  
+                document.getElementById("link").innerHTML = "ADD ROUTE";
+                 
+            } else {
+                tableData.destroy();
+                for (i = 0; i < dataLength+2; i++) {
+                    document.getElementById("dataTable").deleteRow(0);
+                }
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].routeId.route_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].routeId.routeLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].routeName;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId+")\">Delete</button></span>";
+                                break;
+                        }
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD ROUTE";
+            }   
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+}
