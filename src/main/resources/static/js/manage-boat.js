8ca92378bfccea26be2ca6183da9bc40027f8ca8
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-08-30 19:32:17 +0700

    [ADD] schedule to boat management page

--- src/main/resources/static/js/manage-boat.js
+++ src/main/resources/static/js/manage-boat.js
@@ -311,3 +311,131 @@ function changeViewRoute() {
         }
     });
 }
+
+function changeViewSchedule() {
+       $.ajax({
+        type: "GET",
+        url: "http://139.99.117.190:8080/schedules",
+        dataType: 'json',
+        success: function (data) {
+            var dataHeader = ["<b>Schedule ID</b>","<b>Language</b>","<b>Route Name</b>","<b>Day</b>","<b>Time</b>","<b>Price (THB.)</b>","<b>Boat Frequency (mins)</b>","<b>Edit</b>","<b>Delete</b>"];
+            var cellHeader = [];
+            document.getElementById("tableHeader").innerHTML = "Schedule Table";
+            if (document.getElementById("dataTable").rows.length == 0) {
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].scheduleId.schedule_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].scheduleId.scheduleLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].routeSchedule.routeName;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = ""+data[i].day;
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = ""+data[i].time;
+                                break;
+                            case 5:
+                                cellData[j].innerHTML = ""+data[i].price;
+                                break;
+                            case 6:
+                                cellData[j].innerHTML = ""+data[i].boatpertime;
+                                break;
+                            case 7:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].scheduleId+")\">Edit</button></span>";
+                                break;
+                            case 8:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].scheduleId+")\">Delete</button></span>";
+                                break;
+                        } 
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length;  
+                document.getElementById("link").innerHTML = "ADD SCHEDULE";
+                 
+            } else {
+                tableData.destroy();
+                for (i = 0; i < dataLength+2; i++) {
+                    document.getElementById("dataTable").deleteRow(0);
+                }
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].scheduleId.schedule_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].scheduleId.scheduleLanguages.toString();
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].routeSchedule.routeName;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = ""+data[i].day;
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = ""+data[i].time;
+                                break;
+                            case 5:
+                                cellData[j].innerHTML = ""+data[i].price;
+                                break;
+                            case 6:
+                                cellData[j].innerHTML = ""+data[i].boatpertime;
+                                break;
+                            case 7:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].scheduleId+")\">Edit</button></span>";
+                                break;
+                            case 8:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].scheduleId+")\">Delete</button></span>";
+                                break;
+                        }
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD SCHEDULE";
+            }   
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+}
