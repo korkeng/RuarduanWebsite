2980f8613b40f70b7401e53465dcfe98c4a6f1d2
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-08-31 21:38:55 +0700

    [ADD] placetype to place management page

--- src/main/resources/static/js/manage-place.js
+++ src/main/resources/static/js/manage-place.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $.ajax({
         type: "GET",
-        url: "http://139.99.117.190:8080/places",
+        url: "http://139.99.117.190:8080/placetypes",
         dataType: 'json',
         success: function (data) {
-            var dataHeader = ["<b>Place ID</b>","<b>Language</b>","<b>Name</b>","<b>Pic Path</b>","<b>Description</b>","<b>Operation Time</b>","<b>Latitude</b>","<b>Longitude</b>","<b>Location</b>","<b>External Link</b>","<b>Telephone</b>","<b>Transportation</b>","<b>Edit</b>","<b>Delete</b>"];
+            var dataHeader = ["<b>Place Type ID</b>","<b>Language</b>","<b>Name</b>","<b>Edit</b>","<b>Delete</b>"];
             var cellHeader = [];
-            document.getElementById("tableHeader").innerHTML = "Place Table";
+            document.getElementById("tableHeader").innerHTML = "Place Type Table";
             if (document.getElementById("dataTable").rows.length == 0) {
                 var header = document.getElementById("myThHeader");
                 var rowHeader = header.insertRow(0);
@@ -28,53 +28,26 @@ $(document).ready(function() {
                         cellData[j] = row1.insertCell(j);
                         switch(j) {
                             case 0:
-                                cellData[j].innerHTML = ""+data[i].placeId.place_id.toString();
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
                                 break;
                             case 1:
-                                cellData[j].innerHTML = ""+data[i].placeId.placeLanguages;
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
                                 break;
                             case 2:
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = ""+data[i].pic_path;
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = ""+data[i].description;
-                                break;
-                            case 5:
-                                cellData[j].innerHTML = ""+data[i].time_length;
-                                break; 
-                            case 6:
-                                cellData[j].innerHTML = ""+data[i].pos_latitude.toString();
-                                break;
-                            case 7:
-                                cellData[j].innerHTML = ""+data[i].pos_longtitude.toString();
-                                break;
-                            case 8:
-                                cellData[j].innerHTML = ""+data[i].location;
-                                break;
-                            case 9:
-                                cellData[j].innerHTML = ""+data[i].external_link;
-                                break;
-                            case 10:
-                                cellData[j].innerHTML = ""+data[i].tel;
-                                break;
-                            case 11:
-                                cellData[j].innerHTML = ""+data[i].transportation;
-                                break;
-                            case 12:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeId+")\">Edit</button></span>";
-                                break;
-                            case 13:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeId+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACE";
+                document.getElementById("link").innerHTML = "ADD PLACETYPE";
             } else {
                 tableData.destroy();
                 for (i = 0; i < dataLength+2; i++) {
@@ -100,53 +73,26 @@ $(document).ready(function() {
                         cellData[j] = row1.insertCell(j);
                         switch(j) {
                             case 0:
-                                cellData[j].innerHTML = ""+data[i].placeId.place_id.toString();
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
                                 break;
                             case 1:
-                                cellData[j].innerHTML = ""+data[i].placeId.placeLanguages;
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
                                 break;
                             case 2:
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = ""+data[i].pic_path;
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = ""+data[i].description;
-                                break;
-                            case 5:
-                                cellData[j].innerHTML = ""+data[i].time_length;
-                                break; 
-                            case 6:
-                                cellData[j].innerHTML = ""+data[i].pos_latitude.toString();
-                                break;
-                            case 7:
-                                cellData[j].innerHTML = ""+data[i].pos_longtitude.toString();
-                                break;
-                            case 8:
-                                cellData[j].innerHTML = ""+data[i].location;
-                                break;
-                            case 9:
-                                cellData[j].innerHTML = ""+data[i].external_link;
-                                break;
-                            case 10:
-                                cellData[j].innerHTML = ""+data[i].tel;
-                                break;
-                            case 11:
-                                cellData[j].innerHTML = ""+data[i].transportation;
-                                break;
-                            case 12:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeId+")\">Edit</button></span>";
-                                break;
-                            case 13:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeId+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACE";
+                document.getElementById("link").innerHTML = "ADD PLACETYPE";
             }   
         },
         error: function (e) {
@@ -158,6 +104,109 @@ $(document).ready(function() {
 
 var tableData;
 var dataLength;
+
+function changeViewPlaceType() {
+       $.ajax({
+            type: "GET",
+        url: "http://139.99.117.190:8080/placetypes",
+        dataType: 'json',
+        success: function (data) {
+            var dataHeader = ["<b>Place Type ID</b>","<b>Language</b>","<b>Name</b>","<b>Edit</b>","<b>Delete</b>"];
+            var cellHeader = [];
+            document.getElementById("tableHeader").innerHTML = "Place Type Table";
+            if (document.getElementById("dataTable").rows.length == 0) {
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].name;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
+                                break;
+                        } 
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+            } else {
+                tableData.destroy();
+                for (i = 0; i < dataLength+2; i++) {
+                    document.getElementById("dataTable").deleteRow(0);
+                }
+                var header = document.getElementById("myThHeader");
+                var rowHeader = header.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowHeader.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var footer = document.getElementById("myThFooter");
+                var rowFooter = footer.insertRow(0);
+                for (i = 0; i < dataHeader.length; i++) {
+                    cellHeader[i] = rowFooter.insertCell(i);
+                    cellHeader[i].innerHTML = ""+dataHeader[i];
+                }
+                var table = document.getElementById("myThBody");
+                var cellData = [];
+                for (i = data.length-1; i > -1; i--) {
+                    var row1 = table.insertRow(0);
+                    for(j = 0; j < dataHeader.length; j++){     
+                        cellData[j] = row1.insertCell(j);
+                        switch(j) {
+                            case 0:
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
+                                break;
+                            case 1:
+                                cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
+                                break;
+                            case 2:
+                                cellData[j].innerHTML = ""+data[i].name;
+                                break;
+                            case 3:
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                break;
+                            case 4:
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_idd+")\">Delete</button></span>";
+                                break;
+                        } 
+                    }        
+                }
+                tableData = $('#dataTable').DataTable();
+                dataLength = data.length; 
+                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+            }   
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+}
 
 function changeViewPlace() {
        $.ajax({
