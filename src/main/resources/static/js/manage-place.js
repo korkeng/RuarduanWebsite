03f60a0634e128e8d3113d318c0688c3cbb7a442
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-08-31 22:25:04 +0700

    [ADD] comment code structure for view list place

--- src/main/resources/static/js/manage-place.js
+++ src/main/resources/static/js/manage-place.js
@@ -364,3 +364,119 @@ function changeViewPlace() {
         }
     });
 }
+
+
+// function changeViewListPlace() {
+//        $.ajax({
+//             type: "GET",
+//         url: "http://139.99.117.190:8080/placetypes",
+//         dataType: 'json',
+//         success: function (data) {
+//             var dataHeader = ["<b>Place Type ID</b>","<b>Language</b>","<b>Place Type Name</b>","<b>Place ID</b>","<b>Place Name</b>","<b>Edit</b>","<b>Delete</b>"];
+//             var cellHeader = [];
+//             document.getElementById("tableHeader").innerHTML = "List Place with Placetype Table";
+//             if (document.getElementById("dataTable").rows.length == 0) {
+//                 var header = document.getElementById("myThHeader");
+//                 var rowHeader = header.insertRow(0);
+//                 for (i = 0; i < dataHeader.length; i++) {
+//                     cellHeader[i] = rowHeader.insertCell(i);
+//                     cellHeader[i].innerHTML = ""+dataHeader[i];
+//                 }
+//                 var footer = document.getElementById("myThFooter");
+//                 var rowFooter = footer.insertRow(0);
+//                 for (i = 0; i < dataHeader.length; i++) {
+//                     cellHeader[i] = rowFooter.insertCell(i);
+//                     cellHeader[i].innerHTML = ""+dataHeader[i];
+//                 }
+//                 var table = document.getElementById("myThBody");
+//                 var cellData = [];
+//                 for (i = data.length-1; i > -1; i--) {
+//                     var row1 = table.insertRow(0);
+//                     for(j = 0; j < dataHeader.length; j++){     
+//                         cellData[j] = row1.insertCell(j);
+//                         switch(j) {
+//                             case 0:
+//                                 cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
+//                                 break;
+//                             case 1:
+//                                 cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
+//                                 break;
+//                             case 2:
+//                                 cellData[j].innerHTML = ""+data[i].name;
+//                                 break;
+//                             case 3:
+//                                 cellData[j].innerHTML = ""+data[i].listplace[i].placeId.place_id.toString();
+//                                 break;
+//                             case 4:
+//                                 cellData[j].innerHTML = ""+data[i].listplace[i].name;
+//                                 break;
+//                             case 5:
+//                                 cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeId+")\">Edit</button></span>";
+//                                 break;
+//                             case 6:
+//                                 cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeId+")\">Delete</button></span>";
+//                                 break;
+//                         } 
+//                     }        
+//                 }
+//                 tableData = $('#dataTable').DataTable();
+//                 dataLength = data.length; 
+//                 document.getElementById("link").innerHTML = "ADD LIST PLACE";
+//             } else {
+//                 tableData.destroy();
+//                 for (i = 0; i < dataLength+2; i++) {
+//                     document.getElementById("dataTable").deleteRow(0);
+//                 }
+//                 var header = document.getElementById("myThHeader");
+//                 var rowHeader = header.insertRow(0);
+//                 for (i = 0; i < dataHeader.length; i++) {
+//                     cellHeader[i] = rowHeader.insertCell(i);
+//                     cellHeader[i].innerHTML = ""+dataHeader[i];
+//                 }
+//                 var footer = document.getElementById("myThFooter");
+//                 var rowFooter = footer.insertRow(0);
+//                 for (i = 0; i < dataHeader.length; i++) {
+//                     cellHeader[i] = rowFooter.insertCell(i);
+//                     cellHeader[i].innerHTML = ""+dataHeader[i];
+//                 }
+//                 var table = document.getElementById("myThBody");
+//                 var cellData = [];
+//                 for (i = data.length-1; i > -1; i--) {
+//                     var row1 = table.insertRow(0);
+//                     for(j = 0; j < dataHeader.length; j++){     
+//                         cellData[j] = row1.insertCell(j);
+//                         switch(j) {
+//                             case 0:
+//                                 cellData[j].innerHTML = ""+data[i].placeTypeId.placetype_id.toString();
+//                                 break;
+//                             case 1:
+//                                 cellData[j].innerHTML = ""+data[i].placeTypeId.placetypeLanguages;
+//                                 break;
+//                             case 2:
+//                                 cellData[j].innerHTML = ""+data[i].name;
+//                                 break;
+//                             case 3:
+//                                 cellData[j].innerHTML = ""+data[i].listplace[i].placeId.place_id.toString();
+//                                 break;
+//                             case 4:
+//                                 cellData[j].innerHTML = ""+data[i].listplace[i].name;
+//                                 break;
+//                             case 5:
+//                                 cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeId+")\">Edit</button></span>";
+//                                 break;
+//                             case 6:
+//                                 cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeId+")\">Delete</button></span>";
+//                                 break;
+//                         } 
+//                     }        
+//                 }
+//                 tableData = $('#dataTable').DataTable();
+//                 dataLength = data.length; 
+//                 document.getElementById("link").innerHTML = "ADD LIST PLACE";
+//             }   
+//         },
+//         error: function (e) {
+//             console.log("Error:"+e);
+        }
+    });
+}
