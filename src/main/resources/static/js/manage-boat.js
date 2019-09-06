bed74006d1ab235242cf0d2e732f164668ea2a7d
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-09-06 22:52:37 +0700

    [ADD] insert update delete route but not test

--- src/main/resources/static/js/manage-boat.js
+++ src/main/resources/static/js/manage-boat.js
@@ -256,10 +256,10 @@ function changeViewRoute() {
                                 cellData[j].innerHTML = ""+data[i].routeName;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId.route_id+",\'"+data[i].routeId.routeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId.route_id+",\'"+data[i].routeId.routeLanguages+"\')\">Delete</button></span>";
                                 break;
                         } 
                     }        
@@ -304,10 +304,10 @@ function changeViewRoute() {
                                 cellData[j].innerHTML = ""+data[i].routeName;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].routeId.route_id+",\'"+data[i].routeId.routeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].routeId.route_id+",\'"+data[i].routeId.routeLanguages+"\')\">Delete</button></span>";
                                 break;
                         }
                     }        
@@ -546,7 +546,87 @@ function addBoatType(){
 }
 
 function addRoute(){
-   console.log("Hello5"); 
+    var divRoute =
+    '<div id="page-contentid" class="page-content">'+
+        '<div class="form-v5-content">'+
+            '<span class="closeform">&times;</span>'+
+            '<form id="formRoute" class="form-detail">'+
+                '<h2>Route Table</h2>'+
+                '<div class="form-row">'+
+                    '<span>Route Id</span><span style="color: red;"> *</span>'+
+                    '<input id="valId" type="number" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Languages</span><span style="color: red;"> *</span>'+
+                    '<input id="valLan" type="text" class="input-text" placeholder="Ex. TH, ENG" required>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Route Name</span><span style="color: red;"> *</span>'+
+                    '<input id="valName" type="text" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row-last">'+
+                    '<input type="button" id="addDBRoute" class="btn btn-sm" value="Go"/>'+
+                '</div>'+
+            '</form>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById("showPopupForm").innerHTML = divBoatType;
+
+    var modal = document.getElementById("showPopupForm"); 
+    modal.style.display = "block";
+    var spanclose = document.getElementsByClassName("closeform")[0];
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+    var btnclose = document.getElementById("addDBRoute");
+    btnclose.onclick = function() {
+        var objectRoute = {
+            routeId: {
+                route_id: document.getElementById("valId").value,
+                routeLanguages: document.getElementById("valLan").value
+            },
+            name:   document.getElementById("valName").value
+        }
+        swal({
+            title: 'Are you sure?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#32CD32',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, add it!'
+        }).then(function() {
+            if(objectRoute.routeId.route_id != "" && objectRoute.routeId.routeLanguages != "" &&
+                objectRoute.routeName != ""){
+                    $.ajax({
+                        type : "POST",
+                        contentType : "application/json",
+                        url : "http://139.99.117.190:8080/route",
+                        data : JSON.stringify(objectRoute),
+                        dataType : 'json',
+                        success : function() {
+                            swal(
+                                'Successful',
+                                'Your data has been add.',
+                                'success');
+
+                            modal.style.display = "none";
+                            changeViewBoattype();
+                        },
+                        error : function(e) {
+                            alert("Error!")
+                            console.log("ERROR: ", e);
+                        }
+                    });
+            } else {
+                swal(
+                    'Failure',
+                    'Please fill in the textfield.',
+                    'error'
+                );
+            }
+            // alertNoClose.show('Successful');
+        });
 }
 
 function addSchedule(){
@@ -656,6 +736,111 @@ function editBoatType(id,lan){
 
 }
 
+
+function editRoute(id,lan){
+    var divRoute =
+    '<div id="page-contentid" class="page-content">'+
+        '<div class="form-v5-content">'+
+            '<span class="closeform">&times;</span>'+
+            '<form id="formRoute" class="form-detail">'+
+                '<h2>Route Table</h2>'+
+                '<div class="form-row">'+
+                    '<span>Route Id</span>'+
+                    '<input id="valId" type="number" class="input-text" disabled>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Languages</span>'+
+                    '<input id="valLan" type="text" class="input-text" placeholder="Ex. TH, ENG" disabled>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Route Name</span><span style="color: red;"> *</span>'+
+                    '<input id="valName" type="text" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row-last">'+
+                    '<input type="button" id="editDBRoute" class="btn btn-sm" value="Go"/>'+
+                '</div>'+
+            '</form>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById("showPopupForm").innerHTML = divRoute;
+
+    var modal = document.getElementById("showPopupForm"); 
+    modal.style.display = "block";
+    var spanclose = document.getElementsByClassName("closeform")[0];
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+    var btnclose = document.getElementById("editDBRoute");
+
+    $.ajax({
+        type: "GET",
+        url: "http://139.99.117.190:8080/routes/"+lan+"/"+id.toString(),
+        dataType: 'json',
+        success: function (data) {
+            document.getElementById("valId").value = data.routeId.route_id.toString();
+            document.getElementById("valLan").value = data.routeId.routeLanguages;
+            document.getElementById("valName").value = data.routeName;
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+    btnclose.onclick = function() {
+        swal({
+            title: 'Are you sure?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#32CD32',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes!'
+        }).then(function() {
+        var objectRoute = {
+            routeId: {
+                route_id: document.getElementById("valId").value,
+                routeLanguages: document.getElementById("valLan").value
+            },
+            name:   document.getElementById("valName").value
+        }
+            $.ajax({
+                type : "PUT",
+                contentType : "application/json",
+                url : "http://139.99.117.190:8080/route/update/"+lan+"/"+id.toString(),
+                data : JSON.stringify(objectRoute),
+                dataType : 'json',
+                success : function() {
+                    swal({
+                        title: 'Successful!!',
+                        confirmButtonText: 'OK',
+                        text: 'Your data has been update.',
+                        preConfirm: () => {
+                            modal.style.display = "none";
+                            window.location.reload();
+                        }
+                    });
+                },
+                error : function(e) {
+                    swal({
+                        title: 'Failure!!',
+                        confirmButtonText: 'OK',
+                        preConfirm: () => {
+                            modal.style.display = "none";
+                        }
+                    });
+                  console.log("ERROR: ", e);
+                }
+            });
+            // alertNoClose.show('Successful');
+        });
+        // modal.style.display = "none";
+    }
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+
+}
+
+
 function deleteBoatType(id,lan){
     swal({
         title: 'Are you sure?',
@@ -669,6 +854,40 @@ function deleteBoatType(id,lan){
             type : "DELETE",
             contentType : "application/json",
             url : "http://139.99.117.190:8080/boattype/"+lan+"/"+id,
+            dataType : 'json',
+            success : function() {
+                swal(
+                    'Successful',
+                    'Your data has been delete.',
+                    'success');
+                changeViewBoattype();
+            },
+            error : function(e) {
+                swal(
+                    'Failure',
+                    'error'
+                );
+              console.log("ERROR: ", e);
+            }
+        });
+        // alertNoClose.show('Successful');
+    });
+    console.log("Remove");
+}
+
+function deleteRoute(id,lan){
+    swal({
+        title: 'Are you sure?',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#32CD32',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then(function() {
+        $.ajax({
+            type : "DELETE",
+            contentType : "application/json",
+            url : "http://139.99.117.190:8080/route/"+lan+"/"+id,
             dataType : 'json',
             success : function() {
                 swal(
