54ab20e65f0c06d2d78e25b9c4e1f1c81cbcbde8
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-09-07 20:51:23 +0700

    [ADD] insert & delete in placetype management

--- src/main/resources/static/js/manage-place.js
+++ src/main/resources/static/js/manage-place.js
@@ -37,17 +37,19 @@ $(document).ready(function() {
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editPlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deletePlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+                document.getElementById("tableAddData").innerHTML = "<button type=\"button\" class=\"btn\" id=\"addDataButton\" onclick=\"addPlaceType()\">"+
+                                                                    "ADD PLACETYPE"+
+                                                                    "</button>";
             } else {
                 tableData.destroy();
                 for (i = 0; i < dataLength+2; i++) {
@@ -82,17 +84,19 @@ $(document).ready(function() {
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editPlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deletePlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+                document.getElementById("tableAddData").innerHTML = "<button type=\"button\" class=\"btn\" id=\"addDataButton\" onclick=\"addPlaceType()\">"+
+                                                                    "ADD PLACETYPE"+
+                                                                    "</button>";
             }   
         },
         error: function (e) {
@@ -144,17 +148,19 @@ function changeViewPlaceType() {
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editPlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_id+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deletePlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+                document.getElementById("tableAddData").innerHTML = "<button type=\"button\" class=\"btn\" id=\"addDataButton\" onclick=\"addPlaceType()\">"+
+                                                                    "ADD PLACETYPE"+
+                                                                    "</button>";
             } else {
                 tableData.destroy();
                 for (i = 0; i < dataLength+2; i++) {
@@ -189,17 +195,19 @@ function changeViewPlaceType() {
                                 cellData[j].innerHTML = ""+data[i].name;
                                 break;
                             case 3:
-                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editRole("+data[i].placeTypeId.placetype_id+")\">Edit</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-edit\"><button type=\"button\" class=\"btn btn-warning btn-rounded btn-sm my-0\" onclick=\"editPlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Edit</button></span>";
                                 break;
                             case 4:
-                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deleteRole("+data[i].placeTypeId.placetype_idd+")\">Delete</button></span>";
+                                cellData[j].innerHTML = "<span class=\"table-remove\"><button type=\"button\" class=\"btn btn-danger btn-rounded btn-sm my-0\" onclick=\"deletePlaceType("+data[i].placeTypeId.placetype_id+",\'"+data[i].placeTypeId.placetypeLanguages+"\')\">Delete</button></span>";
                                 break;
                         } 
                     }        
                 }
                 tableData = $('#dataTable').DataTable();
                 dataLength = data.length; 
-                document.getElementById("link").innerHTML = "ADD PLACETYPE";
+                document.getElementById("tableAddData").innerHTML = "<button type=\"button\" class=\"btn\" id=\"addDataButton\" onclick=\"addPlaceType()\">"+
+                                                                    "ADD PLACETYPE"+
+                                                                    "</button>";
             }   
         },
         error: function (e) {
@@ -480,3 +488,229 @@ function changeViewPlace() {
 //         }
 //     });
 // }
+
+
+function addPlaceType(){
+
+    var divPlaceType =
+    '<div id="page-contentid" class="page-content">'+
+        '<div class="form-v5-content">'+
+            '<span class="closeform">&times;</span>'+
+            '<form id="formPlaceType" class="form-detail">'+
+                '<h2>PlaceType Table</h2>'+
+                '<div class="form-row">'+
+                    '<span>PlaceType Id</span><span style="color: red;"> *</span>'+
+                    '<input id="valId" type="number" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Languages</span><span style="color: red;"> *</span>'+
+                    '<input id="valLan" type="text" class="input-text" placeholder="Ex. TH, ENG" required>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Name</span><span style="color: red;"> *</span>'+
+                    '<input id="valName" type="text" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row-last">'+
+                    '<input type="button" id="addDBPlaceType" class="btn btn-sm" value="Go"/>'+
+                '</div>'+
+            '</form>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById("showPopupForm").innerHTML = divPlaceType;
+
+    var modal = document.getElementById("showPopupForm"); 
+    modal.style.display = "block";
+    var spanclose = document.getElementsByClassName("closeform")[0];
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+    var btnclose = document.getElementById("addDBPlaceType");
+    btnclose.onclick = function() {
+        var objectPlaceType = {
+            placeTypeId: {
+                placetype_id: document.getElementById("valId").value,
+                placetypeLanguages: document.getElementById("valLan").value
+            },
+            name:   document.getElementById("valName").value
+        }
+        swal({
+            title: 'Are you sure?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#32CD32',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, add it!'
+        }).then(function() {
+            if(objectPlaceType.placeTypeId.placetype_id != "" && objectPlaceType.placeTypeId.placetypeLanguages != "" &&
+                objectPlaceType.name != ""){
+                    $.ajax({
+                        type : "POST",
+                        contentType : "application/json",
+                        url : "https://139.99.117.190:8080/placetype",
+                        data : JSON.stringify(objectPlaceType),
+                        dataType : 'json',
+                        success : function() {
+                            swal(
+                                'Successful',
+                                'Your data has been add.',
+                                'success');
+
+                            modal.style.display = "none";
+                            changeViewPlaceType();
+                        },
+                        error : function(e) {
+                            alert("Error!")
+                            console.log("ERROR: ", e);
+                        }
+                    });
+            } else {
+                swal(
+                    'Failure',
+                    'Please fill in the textfield.',
+                    'error'
+                );
+            }
+            // alertNoClose.show('Successful');
+        });
+    }
+    
+}
+
+function editPlaceType(id,lan){
+    var divPlaceType =
+    '<div id="page-contentid" class="page-content">'+
+        '<div class="form-v5-content">'+
+            '<span class="closeform">&times;</span>'+
+            '<form id="formPlaceType" class="form-detail">'+
+                '<h2>PlaceType Table</h2>'+
+                '<div class="form-row">'+
+                    '<span>PlaceType Id</span>'+
+                    '<input id="valId" type="number" class="input-text" disabled>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Languages</span>'+
+                    '<input id="valLan" type="text" class="input-text" placeholder="Ex. TH, ENG" disabled>'+
+                '</div>'+
+                '<div class="form-row">'+
+                    '<span>Name</span><span style="color: red;"> *</span>'+
+                    '<input id="valName" type="text" class="input-text" required>'+
+                '</div>'+
+                '<div class="form-row-last">'+
+                    '<input type="button" id="editDBPlaceType" class="btn btn-sm" value="Go"/>'+
+                '</div>'+
+            '</form>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById("showPopupForm").innerHTML = divPlaceType;
+
+    var modal = document.getElementById("showPopupForm"); 
+    modal.style.display = "block";
+    var spanclose = document.getElementsByClassName("closeform")[0];
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+    var btnclose = document.getElementById("editDBPlaceType");
+
+    $.ajax({
+        type: "GET",
+        url: "https://139.99.117.190:8080/placetypes/"+lan+"/"+id.toString(),
+        dataType: 'json',
+        success: function (data) {
+            document.getElementById("valId").value = data.placeTypeId.placetype_id.toString();
+            document.getElementById("valLan").value = data.placeTypeId.placetypeLanguages;
+            document.getElementById("valName").value = data.name;
+        },
+        error: function (e) {
+            console.log("Error:"+e);
+        }
+    });
+    btnclose.onclick = function() {
+        swal({
+            title: 'Are you sure?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#32CD32',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes!'
+        }).then(function() {
+        var objectPlaceType = {
+            placeTypeId: {
+                placetype_id: document.getElementById("valId").value,
+                placetypeLanguages: document.getElementById("valLan").value
+            },
+            name:   document.getElementById("valName").value
+        }
+            $.ajax({
+                type : "PUT",
+                contentType : "application/json",
+                url : "https://139.99.117.190:8080/placetype/update/"+lan+"/"+id.toString(),
+                data : JSON.stringify(objectPlaceType),
+                dataType : 'json',
+                success : function() {
+                    swal({
+                        title: 'Successful!!',
+                        confirmButtonText: 'OK',
+                        text: 'Your data has been update.',
+                        preConfirm: () => {
+                            modal.style.display = "none";
+                            window.location.reload();
+                        }
+                    });
+                },
+                error : function(e) {
+                    swal({
+                        title: 'Failure!!',
+                        confirmButtonText: 'OK',
+                        preConfirm: () => {
+                            modal.style.display = "none";
+                        }
+                    });
+                  console.log("ERROR: ", e);
+                }
+            });
+            // alertNoClose.show('Successful');
+        });
+        // modal.style.display = "none";
+    }
+    spanclose.onclick = function() {
+        modal.style.display = "none";
+    }
+
+}
+
+
+function deletePlaceType(id,lan){
+    swal({
+        title: 'Are you sure?',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#32CD32',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then(function() {
+        $.ajax({
+            type : "DELETE",
+            contentType : "application/json",
+            url : "https://139.99.117.190:8080/placetypes/"+lan+"/"+id,
+            dataType : 'json',
+            success : function() {
+                swal(
+                    'Successful',
+                    'Your data has been delete.',
+                    'success');
+                changeViewPlaceType();
+            },
+            error : function(e) {
+                swal(
+                    'Failure',
+                    'error'
+                );
+              console.log("ERROR: ", e);
+            }
+        });
+        // alertNoClose.show('Successful');
+    });
+    console.log("Remove");
+}
