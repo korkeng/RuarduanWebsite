f0d4c39b72350a47b36fdb8dd51a71dd77ff892b
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-10-30 20:33:09 +0700

    [SOLVE] dropdown bug in placetype & [ADD] gg map to add & edit place

--- src/main/resources/static/js/manage-place.js
+++ src/main/resources/static/js/manage-place.js
@@ -491,7 +491,7 @@ function addPlaceType(){
                 '</div>'+
                 '<div class="form-row">'+
                     '<span>Languages</span><span style="color: red;"> *</span>'+
-                    '<select id="valLan" class="btn-xxl text-center input-text" name="locality" onclick="chooseLang()">'+
+                    '<select id="valLan" class="btn-xxl text-center input-text" name="locality">'+
                             '<option selected value="base">==Choose Language==</option>'+
                     '</select>'+
                 '</div>'+
@@ -531,7 +531,22 @@ function addPlaceType(){
         modal.style.display = "none";
     }
     var btnclose = document.getElementById("addDBPlaceType");
+  
+    let dropdown = $('#valLan');
+    var usedNames = [];
+    var listLang = document.getElementById("valLan");
+    const url = 'https://ruarduan-backend.com/placetypes';
+    $.getJSON(url, function (data) {
+      $.each(data, function (key, entry) {
+        if (usedNames.indexOf(entry.placeTypeId.placetypeLanguages) == -1) {
+                $("#valLan").append("<option value=" + key + ">" + entry.placeTypeId.placetypeLanguages + "</option>"); 
+            }
+        usedNames.push(entry.placeTypeId.placetypeLanguages);
+      })
+    });
+
     btnclose.onclick = function() {
+        langName = listLang.options[listLang.selectedIndex].text;
         var objectPlaceType = {
             placeTypeId: {
                 placetype_id: document.getElementById("valId").value,
@@ -977,13 +992,17 @@ function addPlace(){
                     '<span>Operation Time</span><span style="color: red;"> *</span>'+
                     '<input id="valTime" type="text" class="input-text" required>'+
                 '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Latitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLat" type="text" class="input-text" required disabled>'+
+                // '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Longitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLong" type="text" class="input-text" required disabled>'+
+                // '</div>'+
                 '<div class="form-row">'+
-                    '<span>Latitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLat" type="text" class="input-text" required>'+
-                '</div>'+
-                '<div class="form-row">'+
-                    '<span>Longitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLong" type="text" class="input-text" required>'+
+                    '<span>Latitude & Longitude</span><span style="color: red;"> *</span>'+
+                    '<div id="googleMap" style="width:100%;height:400px;margin-bottom: 26px;"></div>'+
                 '</div>'+
                 '<div class="form-row">'+
                     '<span>Location</span><span style="color: red;"> *</span>'+
@@ -1046,6 +1065,25 @@ function addPlace(){
         modal.style.display = "none";
     }
     var btnclose = document.getElementById("addDBPlace");
+    var mapProp= {
+        center:new google.maps.LatLng(13.729295, 100.501006),
+        zoom:12.95,
+    };
+    var maps = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker();
+
+    google.maps.event.addListener(maps, 'click', function(e){
+
+        if(marker.getPosition() != null){
+            marker.setMap(null);
+        }
+        marker = new google.maps.Marker({
+            position: e.latLng,
+            map: maps
+        });
+        // console.log(marker.getPosition().lat().toFixed(4));
+        //  console.log(marker.getPosition().lng().toFixed(4));
+    });
     btnclose.onclick = function() {
         var objectPlace = {
             placeId: {
@@ -1056,8 +1094,10 @@ function addPlace(){
             pic_path: document.getElementById("valPic").value,
             description: document.getElementById("valDes").value,
             time_length: document.getElementById("valTime").value,
-            pos_latitude: parseFloat(document.getElementById("valLat").value),
-            pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            // pos_latitude: parseFloat(document.getElementById("valLat").value),
+            // pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            pos_latitude: parseFloat(marker.getPosition().lat().toFixed(4)),
+            pos_longtitude: parseFloat(marker.getPosition().lng().toFixed(4)),
             location: document.getElementById("valLo").value,
             external_link: document.getElementById("valLink").value,
             Limitation: document.getElementById("valLim").value,
@@ -1189,13 +1229,17 @@ function addPlaceNewLang(id,lan){
                     '<span>Operation Time</span><span style="color: red;"> *</span>'+
                     '<input id="valTime" type="text" class="input-text" required>'+
                 '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Latitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLat" type="text" class="input-text" required disabled>'+
+                // '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Longitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLong" type="text" class="input-text" required disabled>'+
+                // '</div>'+
                 '<div class="form-row">'+
-                    '<span>Latitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLat" type="text" class="input-text" required disabled>'+
-                '</div>'+
-                '<div class="form-row">'+
-                    '<span>Longitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLong" type="text" class="input-text" required disabled>'+
+                    '<span>Latitude & Longitude</span><span style="color: red;"> *</span>'+
+                    '<div id="googleMap" style="width:100%;height:400px;margin-bottom: 26px;"></div>'+
                 '</div>'+
                 '<div class="form-row">'+
                     '<span>Location</span><span style="color: red;"> *</span>'+
@@ -1259,20 +1303,40 @@ function addPlaceNewLang(id,lan){
         modal.style.display = "none";
     }
     var btnclose = document.getElementById("addDBPlace");
+
     $.ajax({
         type: "GET",
         url: "https://ruarduan-backend.com/places/"+lan+"/"+id.toString(),
         dataType: 'json',
         success: function (data) {
             document.getElementById("valId").value = data.placeId.placeid.toString();
-            document.getElementById("valLat").value = data.pos_latitude.toString();
-            document.getElementById("valLong").value = data.pos_longtitude.toString();
+            // document.getElementById("valLat").value = data.pos_latitude.toString();
+            // document.getElementById("valLong").value = data.pos_longtitude.toString();
            
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng( data.pos_latitude, data.pos_longtitude),
+                map: maps,
+                title: ''+data.name,
+                draggable: false
+            });
+            markerLat = data.pos_latitude;
+            markerLong = data.pos_longtitude;
         },
         error: function (e) {
             console.log("Error:"+e);
         }
     });
+
+    var mapProp= {
+        center:new google.maps.LatLng(13.729295, 100.501006),
+        zoom:12.95,
+    };
+    var maps = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var marker;
+    var markerLat;
+    var markerLong;
+
     btnclose.onclick = function() {
         var objectPlace = {
             placeId: {
@@ -1283,13 +1347,16 @@ function addPlaceNewLang(id,lan){
             pic_path: document.getElementById("valPic").value,
             description: document.getElementById("valDes").value,
             time_length: document.getElementById("valTime").value,
-            pos_latitude: parseFloat(document.getElementById("valLat").value),
-            pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            // pos_latitude: parseFloat(document.getElementById("valLat").value),
+            // pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            pos_latitude: parseFloat(markerLat),
+            pos_longtitude: parseFloat(markerLong),
             location: document.getElementById("valLo").value,
             external_link: document.getElementById("valLink").value,
             Limitation: document.getElementById("valLim").value,
             tel: document.getElementById("valTel").value,
             transportation: document.getElementById("valTrans").value
+
         }
         swal({
             title: 'Add Place Data?',
@@ -1386,7 +1453,9 @@ function addPlaceNewLang(id,lan){
     
 }
 
+
 function editPlace(id,lan){
+
     var divPlace =
     '<div id="page-contentid" class="page-content">'+
         '<div class="form-v5-content">'+
@@ -1417,13 +1486,17 @@ function editPlace(id,lan){
                     '<span>Operation Time</span><span style="color: red;"> *</span>'+
                     '<input id="valTime" type="text" class="input-text" required>'+
                 '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Latitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLat" type="text" class="input-text" required disabled>'+
+                // '</div>'+
+                // '<div class="form-row">'+
+                //     '<span>Longitude</span><span style="color: red;"> *</span>'+
+                //     '<input id="valLong" type="text" class="input-text" required disabled>'+
+                // '</div>'+
                 '<div class="form-row">'+
-                    '<span>Latitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLat" type="text" class="input-text" required disabled>'+
-                '</div>'+
-                '<div class="form-row">'+
-                    '<span>Longitude</span><span style="color: red;"> *</span>'+
-                    '<input id="valLong" type="text" class="input-text" required disabled>'+
+                    '<span>Latitude&Longitude</span><span style="color: red;"> *</span>'+
+                    '<div id="googleMap" style="width:100%;height:400px;margin-bottom: 26px;"></div>'+
                 '</div>'+
                 '<div class="form-row">'+
                     '<span>Location</span><span style="color: red;"> *</span>'+
@@ -1484,6 +1557,16 @@ function editPlace(id,lan){
     }
     var btnclose = document.getElementById("editDBPlace");
 
+    var mapProp= {
+        center:new google.maps.LatLng(13.729295, 100.501006),
+        zoom:12.95,
+    };
+    var maps = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var marker;
+    var markerLat;
+    var markerLong;
+
     $.ajax({
         type: "GET",
         url: "https://ruarduan-backend.com/places/"+lan+"/"+id.toString(),
@@ -1495,13 +1578,27 @@ function editPlace(id,lan){
             document.getElementById("valPic").value = data.pic_path;
             document.getElementById("valDes").value = data.description;
             document.getElementById("valTime").value = data.time_length;
-            document.getElementById("valLat").value = data.pos_latitude;
-            document.getElementById("valLong").value = data.pos_longtitude;
+
+            // document.getElementById("valLat").value = data.pos_latitude;
+            // document.getElementById("valLong").value = data.pos_longtitude;
             document.getElementById("valLo").value = data.location;
             document.getElementById("valLink").value = data.external_link;
             document.getElementById("valLim").value = data.limitation;
             document.getElementById("valTel").value = data.tel;
             document.getElementById("valTrans").value = data.transportation;
+
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng( data.pos_latitude, data.pos_longtitude),
+                map: maps,
+                title: ''+data.name,
+                draggable: false
+            });
+            markerLat = data.pos_latitude;
+            markerLong = data.pos_longtitude;
+            // google.maps.event.addListener(marker, 'dragend', function (evt) {
+            //     newMarkerLat = evt.latLng.lat().toFixed(6);
+            //     newMarkerLong = evt.latLng.lng().toFixed(6);
+            // });
         },
         error: function (e) {
             console.log("Error:"+e);
@@ -1525,8 +1622,10 @@ function editPlace(id,lan){
             pic_path: document.getElementById("valPic").value,
             description: document.getElementById("valDes").value,
             time_length: document.getElementById("valTime").value,
-            pos_latitude: parseFloat(document.getElementById("valLat").value),
-            pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            // pos_latitude: parseFloat(document.getElementById("valLat").value),
+            // pos_longtitude: parseFloat(document.getElementById("valLong").value),
+            pos_latitude: parseFloat(markerLat),
+            pos_longtitude: parseFloat(markerLong),
             location: document.getElementById("valLo").value,
             external_link: document.getElementById("valLink").value,
             Limitation: document.getElementById("valLim").value,
