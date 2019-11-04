53c84abdcbc7a6d20be4bf18460b6e1189940957
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-11-04 09:29:57 +0700

    test login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,138 +1,5 @@
 
-// $(document).ready(function() {
-//     "use strict";
-    
-//     var input = $('.validate-input .input100');
-    
-//     $('.validate-form').submit( function () {
-        
-//         var login = {};
-//         login["username"] = $("#username").val();
-//         login["password"] = $("#password").val();
-//         window.localStorage.setItem("dataUsername",login.username);
-//     });
-
-//     $.ajax({
-//             type : "GET",
-//             contentType : "application/json",
-//             url : "https://ruarduan-backend.com/account/"+window.localStorage.getItem("dataUsername"),
-//             dataType : 'json',
-//             success : function(data) {
-//                 window.localStorage.setItem("adminId",data.accountId);
-//             },
-//             error : function(e) {
-//               console.log("ERROR: ", e);
-//             }
-//         });
-
-//     $('.validate-form').on('submit',function(){
-//         var check = true;
-//         for(var i=0; i<input.length; i++) {
-//             if(validate(input[i]) == false){
-//                 showValidate(input[i]);
-//                 check=false;
-//             }
-
-//         }
-//         return check;
-//     });
-
-//     $('.validate-form .input100').each(function(){
-//         $(this).focus(function(){
-//            hideValidate(this);
-//         });
-//     });
-
-//     function validate (input) {
-//         if($(input).attr('type') == 'text' || $(input).attr('id') == 'username') {
-//             if($(input).val().trim() == null || $(input).val().trim() == '') {
-//                 return false;
-//             }
-//         }
-//         else {
-//             if($(input).val().trim() == ''){
-//                 return false;
-//             }
-//         }
-//     }
-
-//     function showValidate(input) {
-//         var thisAlert = $(input).parent();
-
-//         $(thisAlert).addClass('alert-validate');
-//     }
-
-//     function hideValidate(input) {
-//         var thisAlert = $(input).parent();
-
-//         $(thisAlert).removeClass('alert-validate');
-//     }
-    
-//     alert("HI")
-    
-
-// });
-
-
-var user;
-var pass;
-
-$(document).ready( function () {
-
-    $('.validate-form').submit( function (event) {
-        event.preventDefault();
-        
-        fire_ajax_submit();
-    });
-});
-
-function fire_ajax_submit() {
-    var login = {};
-    login["username"] = $("#username").val();
-
-     $.ajax({
-            type : "GET",
-            contentType : "application/json",
-            url : "https://ruarduan-backend.com/account/"+window.localStorage.getItem("dataUsername"),
-            dataType : 'json',
-            success : function(data) {
-                window.localStorage.setItem("adminId",data.accountId);
-            },
-            error : function(e) {
-              console.log("ERROR: ", e);
-            }
-        });
-
-}
-$(function(){
-
-    $('#logouttime').show();
-
-    setTimeout(function(){
-        $('#logouttime').fadeOut('slow');
-    },1000);
-
-});
-$(function(){
-
-    $('#invaildtime').show();
-
-    setTimeout(function(){
-        $('#invaildtime').fadeOut('slow');
-    },1000);
-
-});
-// var input = $('.validate-input .input100');
-    
-    // $('.validate-form').submit( function () {
-        
-    //     var login = {};
-    //     login["username"] = $("#username").val();
-    //     login["password"] = $("#password").val();
-    //     window.localStorage.setItem("dataUsername",login.username);
-    // });
-
-(function ($) {
+$(document).ready(function() {
     "use strict";
     
     var input = $('.validate-input .input100');
@@ -145,6 +12,18 @@ $(function(){
         window.localStorage.setItem("dataUsername",login.username);
     });
 
+    $.ajax({
+            type : "GET",
+            contentType : "application/json",
+            url : "https://ruarduan-backend.com/account/"+window.localStorage.getItem("dataUsername"),
+            dataType : 'json',
+            success : function(data) {
+                window.localStorage.setItem("adminId",data.accountId);
+            },
+            error : function(e) {
+              console.log("ERROR: ", e);
+            }
+        });
 
     $('.validate-form').on('submit',function(){
         var check = true;
@@ -189,7 +68,127 @@ $(function(){
         $(thisAlert).removeClass('alert-validate');
     }
     
+    
+
+});
+
+
+// var user;
+// var pass;
+
+// $(document).ready( function () {
+
+//     $('.validate-form').submit( function (event) {
+//         event.preventDefault();
+        
+//         fire_ajax_submit();
+//     });
+// });
+
+// function fire_ajax_submit() {
+//     var login = {};
+//     login["username"] = $("#username").val();
+
+//      $.ajax({
+//             type : "GET",
+//             contentType : "application/json",
+//             url : "https://ruarduan-backend.com/account/"+window.localStorage.getItem("dataUsername"),
+//             dataType : 'json',
+//             success : function(data) {
+//                 window.localStorage.setItem("adminId",data.accountId);
+//             },
+//             error : function(e) {
+//               console.log("ERROR: ", e);
+//             }
+//         });
+
+// }
+// $(function(){
+
+//     $('#logouttime').show();
+
+//     setTimeout(function(){
+//         $('#logouttime').fadeOut('slow');
+//     },1000);
+
+// });
+// $(function(){
+
+//     $('#invaildtime').show();
+
+//     setTimeout(function(){
+//         $('#invaildtime').fadeOut('slow');
+//     },1000);
+
+// });
+// var input = $('.validate-input .input100');
+    
+    // $('.validate-form').submit( function () {
+        
+    //     var login = {};
+    //     login["username"] = $("#username").val();
+    //     login["password"] = $("#password").val();
+    //     window.localStorage.setItem("dataUsername",login.username);
+    // });
+
+// (function ($) {
+//     "use strict";
+    
+//     var input = $('.validate-input .input100');
+    
+//     $('.validate-form').submit( function () {
+        
+//         var login = {};
+//         login["username"] = $("#username").val();
+//         login["password"] = $("#password").val();
+//         window.localStorage.setItem("dataUsername",login.username);
+//     });
+
+
+//     $('.validate-form').on('submit',function(){
+//         var check = true;
+//         for(var i=0; i<input.length; i++) {
+//             if(validate(input[i]) == false){
+//                 showValidate(input[i]);
+//                 check=false;
+//             }
+
+//         }
+//         return check;
+//     });
+
+//     $('.validate-form .input100').each(function(){
+//         $(this).focus(function(){
+//            hideValidate(this);
+//         });
+//     });
+
+//     function validate (input) {
+//         if($(input).attr('type') == 'text' || $(input).attr('id') == 'username') {
+//             if($(input).val().trim() == null || $(input).val().trim() == '') {
+//                 return false;
+//             }
+//         }
+//         else {
+//             if($(input).val().trim() == ''){
+//                 return false;
+//             }
+//         }
+//     }
+
+//     function showValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).addClass('alert-validate');
+//     }
+
+//     function hideValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).removeClass('alert-validate');
+//     }
+    
 
     
 
-})(jQuery);
+// })(jQuery);
