69baead8cf982da3fa6dd347fb9ea0e8bf60bf0e
DESKTOP-QK54PQ8\acer <sawaros.sit@mail.kmutt.ac.th>
2019-11-04 16:17:21 +0700

    [EDIT] successful restructure register

--- src/main/resources/static/js/add-registerToDB.js
+++ src/main/resources/static/js/add-registerToDB.js
@@ -1,3 +1,91 @@
+
+$(document).ready(function() {
+    "use strict";
+    
+    var input = $('.validate-input .input100');
+    
+    $('.validate-form').submit( function () {
+        var register = {};
+        register["Emp_code"] = $("#Emp_code").val();
+        register["FirstName"] = $("#FirstName").val();
+        register["LastName"] = $("#LastName").val();
+        register["Email"] = $("#Email").val();
+        register["Phone"] = $("#Phone").val();
+        register["Username"] = $("#Username").val();
+        register["Password"] = $("#Password").val();
+        console.log("hi")
+        
+    });
+
+    
+    $('.validate-form').on('submit',function(){
+        console.log("Hello");
+        console.log(""+input.length);
+        var check = true;
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+
+        }
+        return check;
+    });
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'text' || $(input).attr('id') == 'Emp_code') {
+            if($(input).val().trim() == null || $(input).val().trim() == '') {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    $(function(){
+
+        $('#logouttime').show();
+
+        setTimeout(function(){
+            $('#logouttime').fadeOut('slow');
+        },1000);
+
+    });
+
+    $(function(){
+
+        $('#invaildtime').show();
+
+        setTimeout(function(){
+            $('#invaildtime').fadeOut('slow');
+        },1000);
+
+    });
+    
+});
+
+
 var header = {
     "alg": "HS256",
     "typ": "JWT"
@@ -25,7 +113,8 @@ var header = {
   }
   
 function addAccount(){
-
+    // var password = $("#Password").val();
+    // var confirmPassword = $("#ConfirmPassword").val();
     var userData = {
         password: document.getElementById("Password").value,
     };
@@ -81,7 +170,7 @@ function addAccount(){
                     error : function(e) {
                       swal("Something went Wrong!","", "error");
                       console.log("ERROR: ", e);
-z                    }
+                    }
                 });
     }else{
         swal("Please Fill All Form!", "", "warning");
